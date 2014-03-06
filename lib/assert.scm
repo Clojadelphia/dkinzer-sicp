5ac61d5fe01d609693dfabae2bd01c6c24206a4c
David Kinzer <dtkinzer@gmail.com>
2014-03-06 09:29:21 -0500

    Add time analysis function.

--- lib/assert.scm
+++ lib/assert.scm
@@ -1,5 +1,17 @@
+; @file
+; assert.scm
+; Testing and analysis type functions go here.
+
 (define assert
   (lambda (a m)
     (if a 
       (display (string "\n" "PASSED: " m "\n"))
       (display (string "\n" "FAILED: " m "\n")))))
+
+; Use (time f) to analyse how long procedures take.
+; f is an unevaluated expression.
+(define time
+  (lambda (f)
+    (define t (process-time-clock))
+    (f)
+    (- (process-time-clock) t)))
