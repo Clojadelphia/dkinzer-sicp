863685a6b66c26cfbe425de30920df0426bb96ca
David Kinzer <dtkinzer@gmail.com>
2014-03-12 19:44:30 -0400

    Fix exercise 1.9a

--- src/1.2.scm
+++ src/1.2.scm
@@ -5,24 +5,25 @@
 ; integers in terms of the procedures inc, which increments its argument by 1,
 ; and dec, which decrements its argument by 1.
 
- (define (+1.9a a b)
-   (if (= a 0)
-     b
-     (inc (+ (dec a) b))))
+(define (+1.9a a b)
+  (if (= a 0)
+    b
+    (inc (+1.9a (dec a) b))))
 
- (define (+1.9b a b)
-   (if (= a 0)
-     b
-    (+ (dec a)  (inc b))))
+(define (+1.9b a b)
+  (if (= a 0)
+    b
+    (+1.9b (dec a)  (inc b))))
+
 (set-gc-notification! #t)
 
-(display (string "TEST time for : (+1.9a 10000000000000.0e100 10000e100)"))
+(display (string "TEST time for : (+1.9a 1e4 1e4)"))
 (define +1.9a-times
-  (time (lambda () (+1.9a 10000000000000.0e100 10000e100))))
+  (time (lambda () (+1.9a 1e4 1e4))))
 
-(display (string "TEST time for : (+1.9b 10000000000000.0e100 10000e100)"))
+(display (string "TEST time for : (+1.9b 1e4 1e4)"))
 (define +1.9b-times
-  (time (lambda () (+1.9b 10000000000000.0e100 10000e100))))
+  (time (lambda () (+1.9b 1e4 1e4))))
 
 (assert (= +1.9a-times +1.9b-times)
         "There are no time differences between procedure +1.9a and +1.9b.")
