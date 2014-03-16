1a5edebe279c060eb0cb005a33445b76442157e3
David Kinzer <dtkinzer@gmail.com>
2014-03-16 19:35:11 -0400

    Fix Travis run.

--- bin/run.sh
+++ bin/run.sh
@@ -14,7 +14,7 @@ else
   FILE_TYPE=scm
 fi
 
-for file in $(find src -type f -name "*.$FILE_TYPE")
+for file in $(find ./src -type f -name "*.$FILE_TYPE")
 do
   echo -e "$Yel Running Problem Set: $file $RCol"
   echo ""
