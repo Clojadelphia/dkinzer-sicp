45d73f5c6a6e1e0c854d533762f9d037e878a91f
David Kinzer <dtkinzer@gmail.com>
2014-04-20 22:57:33 -0400

    1.3.2

--- src/1.3.scm
+++ src/1.3.scm
@@ -27,3 +27,17 @@
 
 (assert (= (sympson-integral cube 0 1 100) .24999999999999992)
         "sympson-integral of cube works as expected.")
+
+; Exercise 1.30:
+; The =sum= procedure above generates a linear recursion.  The procedure can be
+; rewritten so that the sum is performed iteratively.  Show how to do this by
+; filling in the missing expressions in the following definition:
+(define (sum-130 term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(assert (= (sum-130 cube 1 inc 10) (sum cube 1 inc 10))
+        "The iterative procedure sum-130 is equivalent recursive procedure sum.")
