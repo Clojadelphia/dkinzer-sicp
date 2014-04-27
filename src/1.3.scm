ca74058d959bc7d92422ba221508a860e653d1f1
David Kinzer <dtkinzer@gmail.com>
2014-04-26 22:10:58 -0400

    1.38

--- src/1.3.scm
+++ src/1.3.scm
@@ -456,3 +456,22 @@
         "The recursive and iterative versions of cont-frac are equivalent.
         The iterative version required more thinking becuase I needed to reverse
         the order of the calculations.")
+
+; Exercise 1.38:
+; In 1737, the Swiss mathematician Leonhard Euler published
+; a memoir De Fractionibus Continuis, which included a continued fraction
+; expansion for e - 2, where e is the base of the natural logarithms. In this
+; fraction, the Ni are all 1, and the Di are successively 1, 2, 1, 1, 4, 1, 1,
+; 6, 1, 1, 8, .... Write a program that uses your cont-frac procedure from
+; exercise 1.37 to approximate e, based on Euler's expansion
+
+(define (e-minus-2 k)
+  (cont-frac (lambda (i) 1.0)
+               (lambda (i)
+                 (cond ((= 1 i) 1.0)
+                       ((= 2 i) 2.0)
+                       ((= 0 (modulo (- i 2) 3)) (* 2.0 (/ (+ i 3.0) 3.0)))
+                       (else 1.0)))
+               k))
+
+(define e (+ 2 (e-minus-2 25)))
