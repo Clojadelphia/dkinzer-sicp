3268c948ab52a0aa67ecd73e4ff75ddaf7b0765f
David Kinzer <dtkinzer@gmail.com>
2014-04-29 19:48:40 -0400

    1.41

--- src/1.3.scm
+++ src/1.3.scm
@@ -552,3 +552,18 @@
 
 (assert (is-cubic-root? -3 (cubic 6 11 6))
         "-3 is a root of x^3 + 6x^2 + 11x + 6")
+
+; Exercise 1.41:
+; Define a procedure double that takes a procedure of one
+; argument as argument and returns a procedure that applies the original
+; procedure twice. For example, if inc is a procedure that adds 1 to its
+; argument, then (double inc) should be a procedure that adds 2. What value is
+; returned by
+;
+; (((double (double double)) inc) 5)
+(define (double f)
+  (lambda (x) (f (f x))))
+
+(assert (= 21 (((double (double double)) inc) 5))
+        ; (2^2)^2 = 2^4 = 16
+        "#double works as expected.")
