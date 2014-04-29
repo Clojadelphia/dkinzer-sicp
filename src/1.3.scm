848ee15398acb7864a95bc22b9f6a8fec6d551f0
David Kinzer <dtkinzer@gmail.com>
2014-04-29 19:56:08 -0400

    1.43

--- src/1.3.scm
+++ src/1.3.scm
@@ -567,3 +567,16 @@
 (assert (= 21 (((double (double double)) inc) 5))
         ; (2^2)^2 = 2^4 = 16
         "#double works as expected.")
+
+; Exercise 1.42:
+; Let f and g be two one-argument functions. The composition f after g is
+; defined to be the function x  f(g(x)). Define a procedure compose that
+; implements composition. For example, if inc is a procedure that adds 1 to its
+; argument,
+;
+; ((compose square inc) 6) 49
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(assert (= 49 ((compose square inc) 6))
+        "#compose works as expected.")
