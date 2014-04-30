0d96a15af570b72482e68a0f7e9f8847ba9e83e9
David Kinzer <dtkinzer@gmail.com>
2014-04-29 20:12:48 -0400

    1.43

--- src/1.3.scm
+++ src/1.3.scm
@@ -580,3 +580,24 @@
 
 (assert (= 49 ((compose square inc) 6))
         "#compose works as expected.")
+
+; Exercise 1.43:
+; If f is a numerical function and n is a positive integer, then we can form
+; the nth repeated application of f, which is defined to be the function whose
+; value at x is f(f(...(f(x))...)). For example, if f is the function
+; x -> x + 1, then the nth repeated application of f is the function x -> x + n.
+; If f is the operation of squaring a number, then the nth repeated application
+; of f is the function that raises its argument to the 2nth power. Write
+; a procedure that takes as inputs a procedure that computes f and a positive
+; integer n and returns the procedure that computes the nth repeated
+; application of f. Your procedure should be able to be used as follows:
+;
+; ((repeated square 2) 5) 625
+;
+; Hint: You may find it convenient to use compose from exercise 1.42.
+(define (repeated f n)
+  (if (= 0 n)
+    f
+    (compose f f)))
+(assert (= 625 ((repeated square 2) 5))
+        "The #repeated procedure works as expected.")
