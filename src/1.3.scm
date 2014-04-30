20ad53ca246c0ba6232d31d2bf178a5d2d9897ae
David Kinzer <dtkinzer@gmail.com>
2014-04-30 14:36:39 -0400

    Fix 1.46.
    The procedure #repeated was not working for any value greater than 2.

--- src/1.3.scm
+++ src/1.3.scm
@@ -595,16 +595,25 @@
 ;
 ; Hint: You may find it convenient to use compose from exercise 1.42.
 (define (repeated f n)
-  (if (= 0 n)
-    f
-    (compose f f)))
+  (define (repeat g n)
+    (if (<= n 1)
+      g
+      (repeat (compose g f) (dec n))))
+  (repeat f n))
+
 (assert (= 625 ((repeated square 2) 5))
-        "The #repeated procedure works as expected.")
+        "The #repeated procedure works as expected: (5^2)^2 = 625.")
+
+(assert (= 256 ((repeated square 3) 2))
+        "The #repeated procedure works as expected: (2^2)^2)^2 = 256 ")
+
+(assert (= 256 ((repeated square 3) 2))
+        "The #repeated procedure works as expected: (2^2)^2)^2 = 256 ")
 
 ; Exercise 1.44:
 ; The idea of smoothing a function is an important concept in signal
 ; processing. If f is a function and dx is some small number, then the smoothed
-; version of f is the function whose value at a point x is the average of 
+; version of f is the function whose value at a point x is the average of
 ; f(x - dx), f(x), and f(x + dx). Write a procedure smooth that takes as input
 ; a procedure that computes f and returns a procedure that computes the
 ; smoothed f. It is sometimes valuable to repeatedly smooth a function (that
@@ -612,7 +621,7 @@
 ; function. Show how to generate the n-fold smoothed function of any given
 ; function using smooth and repeated from exercise 1.43.
 (define (smooth f)
-  (lambda(x) 
+  (lambda(x)
     (/
       (+ (f (+ x dx))
          (f x)
