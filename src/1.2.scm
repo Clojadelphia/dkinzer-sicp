d08a309faa66dbde1f57478ac9abca1aeab50cc7
David Kinzer <dtkinzer@gmail.com>
2014-05-02 07:47:12 -0400

    Add 1.12 question.

--- src/1.2.scm
+++ src/1.2.scm
@@ -167,3 +167,16 @@
         "The procedure #fi works as expected for values not less than 3.")
 (assert (= (f 5) (fi 5))
         "The recursive and iterative procedures #f and #fi are equivalent.")
+
+; Exercise 1.12:
+; The following pattern of numbers is called Pascal's triangle.
+;
+;          1
+;        1   1
+;      1   2   1
+;    1   3   3   1
+;  1   4   6   4   1
+;
+; The numbers at the edge of the triangle are all 1, and each number inside the
+; triangle is the sum of the two numbers above it.  Write a procedure that
+; computes elements of Pascal's triangle by means of a recursive process.
