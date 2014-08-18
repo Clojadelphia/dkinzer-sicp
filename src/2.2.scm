b8a92bff04266f85a02165dc8223426770a83e94
David Kinzer <dtkinzer@gmail.com>
2014-08-18 15:03:06 -0400

    Complete 2.26

--- src/2.2.scm
+++ src/2.2.scm
@@ -377,6 +377,16 @@
 ;           (list x y)
 ;
 ; {{{3 Solution
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+(assert (equal? (list 1 2 3 4 5 6) (append x y))
+        "(append x y) results in (1 2 3 4 5 6).")
+
+(assert (equal? (list (list 1 2 3) 4 5 6) (cons x y))
+        "(cons x y) results in ((1 2 3) 4 5 6).")
+
+(assert (equal? (list (list 1 2 3) (list 4 5 6)) (list x y))
+        "(list x y) results in ((1 2 3) (4 5 6).")
 ; {{{2 Exercise 2.27:
 ; {{{3 Problem
 ;      Modify your `reverse' procedure of *Note Exercise
