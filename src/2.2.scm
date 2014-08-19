83d62a3d660a45c63b93510afa75ec481bbfd61e
David Kinzer <dtkinzer@gmail.com>
2014-08-19 09:24:52 -0400

    Complete 2.30

--- src/2.2.scm
+++ src/2.2.scm
@@ -636,6 +636,17 @@
 ;      higher-order procedures) and also by using `map' and recursion.
 ;
 ; {{{3 Solution
+(define (square-tree a-tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (square-tree sub-tree)
+           (* sub-tree sub-tree)))
+       a-tree))
+
+(define x (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+(define y (list 1 (list 4 (list 9 16) 25) (list 36 49)))
+(assert (equal? y (square-tree x))
+        "(square-tree (1 (2 ( 3 4) 5 (6 7)))) equals (1 (4 (9 16) 25 (36 42)")
 ; {{{2 Exercise 2.31:
 ; {{{3 Problem
 ;      Abstract your answer to *Note Exercise 2-30:: to
